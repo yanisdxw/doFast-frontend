8b7ef1a56234e0461cd98a59819e04c7bb0c1276
yanisdxw <yanisdxw@gmail.com>
2023-09-07 16:48:51 +0800

    add component api examples

--- src/components/Todolist.vue
+++ src/components/Todolist.vue
@@ -1,19 +1,61 @@
 <template>
     <div>
-      <button @click="add">{{count}}</button>
+      <input type="text" v-model="title" @keydown.enter="addTodo" />
+      <button v-if="active < all" @click="clear">清理</button>
+      <ul v-if="todos.length">
+        <li v-for="todo in todos">
+          <input type="checkbox" v-model="todo.done" />
+          <span :class="{ done: todo.done }"> {{ todo.title }}</span>
+        </li>
+      </ul>
+      <div v-else>暂无数据</div>
+    <div>
+        全选<input type="checkbox" v-model="allDone" />
+        <span> {{ all - active }} / {{ all }} </span>
+    </div>
+    </div>
+    <div>
+        鼠标位置:<span :class="{x:x, y:y}">{{ x }},{{ y }}</span>
     </div>
   </template>
   
-  <script setup>
-  import { ref } from "vue";
-  let count = ref(1)
-  function add(){
-      count.value++
-  }
-  </script>
-  
-  <style>
-  button {
-    color: red;
-  }
-  </style>
+<script setup>
+
+    import { ref,computed } from "vue";
+    import {useMouse} from '../utils/mouse';
+
+    let {x,y} = useMouse();
+
+    let { title, todos, addTodo, clear, active, all, allDone } = useTodos();
+
+    function useTodos() {
+    let title = ref("");
+    let todos = ref([{ title: "学习Vue", done: false }]);
+    function addTodo() {
+        todos.value.push({
+        title: title.value,
+        done: false,
+        });
+        title.value = "";
+    }
+    function clear() {
+        todos.value = todos.value.filter((v) => !v.done);
+    }
+    let active = computed(() => {
+        return todos.value.filter((v) => !v.done).length;
+    });
+    let all = computed(() => todos.value.length);
+    let allDone = computed({
+        get: function () {
+            return active.value === 0;
+        },
+        set: function (value) {
+        todos.value.forEach((todo) => {
+            todo.done = value;
+        });
+        },
+    });
+    return { title, todos, addTodo, clear, active, all, allDone };
+    }
+
+</script>
--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,6 +1,11 @@
 <template>
     <h1>这是首页</h1>
     <Todolist/>
+    <Counter/>
 </template>
 
-<script setup> import Todolist from '../components/Todolist.vue' </script>
+<script setup> 
+import Todolist from '../components/Todolist.vue'; 
+import Counter from '../components/Counter.vue';
+
+</script>
