02fcfaa2e6c82a5e910f0a4568968c75af120d4b
yanisdxw <yanisdxw@gmail.com>
2023-09-07 15:19:21 +0800

    add components use example

---
+++ src/components/Todolist.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+      <button @click="add">{{count}}</button>
+    </div>
+  </template>
+  
+  <script setup>
+  import { ref } from "vue";
+  let count = ref(1)
+  function add(){
+      count.value++
+  }
+  </script>
+  
+  <style>
+  button {
+    color: red;
+  }
+  </style>
--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,3 +1,6 @@
 <template>
     <h1>这是首页</h1>
+    <Todolist/>
 </template>
+
+<script setup> import Todolist from '../components/Todolist.vue' </script>
